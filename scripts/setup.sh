9b4bf7a987ec67dab8d78dd2ca93050ec485715c
sukieraownworld-max <sukieraownworld@gmail.com>
2026-02-24 21:30:39 +0530

    Create setup.sh script

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Setup script to install necessary dependencies
+
+# Update package list
+apt-get update
+
+# Install necessary packages
+apt-get install -y package1 package2 package3
+
+# Any other setup tasks can be added here
